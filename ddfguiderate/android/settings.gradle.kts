05447dea1e8f54dedaa11780bef49afa78c7be30
nevial <mail@nevial.de>
2025-05-07 13:38:33 +0200

    change syntax to kotlin

--- ddfguiderate/android/settings.gradle.kts
+++ ddfguiderate/android/settings.gradle.kts
@@ -19,22 +19,22 @@ include(":app")
 
 // Flutter configuration
 val flutterProjectRoot = rootProject.projectDir.parentFile
-val plugins = new File(flutterProjectRoot, '.flutter-plugins')
-if (plugins.exists()) {
-    plugins.eachLine { line ->
-        def (name, path) = line.split('=')
+val pluginsFile = File(flutterProjectRoot, ".flutter-plugins")
+if (pluginsFile.exists()) {
+    pluginsFile.readLines().forEach { line ->
+        val (name, path) = line.split("=")
         include(":$name")
-        project(":$name").projectDir = new File(path)
+        project(":$name").projectDir = File(path)
     }
 }
 
 // Flutter SDK path
-val localProperties = new File(flutterProjectRoot, "local.properties")
-if (localProperties.exists()) {
-    Properties properties = new Properties()
-    localProperties.withInputStream { properties.load(it) }
-    val flutterSdkPath = properties.getProperty('flutter.sdk')
+val localPropertiesFile = File(flutterProjectRoot, "local.properties")
+if (localPropertiesFile.exists()) {
+    val properties = java.util.Properties()
+    localPropertiesFile.inputStream().use { properties.load(it) }
+    val flutterSdkPath = properties.getProperty("flutter.sdk")
     if (flutterSdkPath != null) {
-        apply from: "$flutterSdkPath/packages/flutter_tools/gradle/app_plugin_loader.gradle"
+        apply(from = "$flutterSdkPath/packages/flutter_tools/gradle/app_plugin_loader.gradle")
     }
 }
